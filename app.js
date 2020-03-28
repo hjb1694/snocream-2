1c20d2f15f56257c34a96b56fb34834edc711db1
Hayden Bradfield <hbradfield20@gmail.com>
2020-03-27 22:34:08 -0400

    added express-session package, initialized express

---
+++ app.js
@@ -0,0 +1,7 @@
+const config = require('./config');
+const express = require('express');
+const app = express();
+
+
+
+app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
