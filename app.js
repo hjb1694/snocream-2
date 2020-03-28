37a32feff3e4be3533642b079e36d640c10c6676
Hayden Bradfield <hbradfield20@gmail.com>
2020-03-28 00:00:29 -0400

    worked on maintenance view and reinstalled sass

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+const path = require('path');
 const config = require('./config');
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// If changes are made to maintenance mode, you must restart the running app!
+app.use((req,res,next) => {
+    if(config.maintenanceMode.toLowerCase() === 'on')
+        return res.status(200).render('maintenance');
+    next();
+});
+
+app.use('/', require('./routes/general'));
+
+app.use('*', (req,res) => res.status(404).redirect('/'));
 
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
     env : process.env.NODE_ENV || 'development', 
-    maintenance : process.env.MAINTENANCE_MODE || 'off', 
+    maintenanceMode : process.env.MAINTENANCE_MODE || 'off', 
     port : process.env.PORT || 8000
 }
